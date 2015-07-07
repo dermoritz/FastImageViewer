9b204bf343debfb5af038f0bccc3384706e1f6fb
ta <tanteanni@hotmail.com>
2015-07-07 12:37:09 +0200

    initial commit

---
+++ src/main/java/de/moritz/fastimageviewer/main/Main.java
@@ -0,0 +1,29 @@
+package de.moritz.fastimageviewer.main;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction((event) -> System.out.println("Hello World!"));
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+		
+	}
+
+}

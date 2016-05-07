a36e68a93d5c66f4de04dc6a0a7fb5a7f5ee9c8c
moritz <moritz.loe@gmail.com>
2016-05-07 10:30:42 +0200

    window maximized on start, image fits

--- src/main/java/de/moritz/fastimageviewer/main/Main.java
+++ src/main/java/de/moritz/fastimageviewer/main/Main.java
@@ -23,21 +23,14 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
 		fxmlLoader = new FXMLLoader(getClass().getResource("/layout.fxml"));
 		MainController controller = i.getInstance(MainController.class);
 		fxmlLoader.setController(controller);
-		primaryStage.setOnShown(event -> controller.onReady());
 		Scene scene = new Scene(fxmlLoader.load());
-		Screen screen = Screen.getPrimary();
-		Rectangle2D bounds = screen.getVisualBounds();
-		primaryStage.setX(0);
-		primaryStage.setY(0);
-		primaryStage.setWidth(bounds.getWidth());
-		primaryStage.setHeight(bounds.getHeight());
 		primaryStage.setScene(scene);
+		primaryStage.setMaximized(true);
 		primaryStage.show();
-
+		controller.onReady();
 	}
 
 }
